1d6e6d250f371175e13a6da0483342ff7da464ac
Lucie Sundermeijer <lucie.sun@gmail.com>
2022-08-16 13:53:47 +0200

    display cart info

--- front/js/cart.js
+++ front/js/cart.js
@@ -19,7 +19,7 @@ function searchProductInfo(id, color, quantity){
     })
     .then(function(product){
         singleProduct = product;   
-             
+        displayCart(id, color, quantity);     
         
     })
     .catch(function(error){
@@ -27,5 +27,73 @@ function searchProductInfo(id, color, quantity){
     })
 }
 
+function displayCart(id, color, quantity){
+    console.log(id, color, quantity, singleProduct.name, singleProduct.price);
+    let newItemArticle = document.createElement("article");
+    newItemArticle.classList.add("cart__item");
+    newItemArticle.setAttribute("data-id", id);
+    newItemArticle.setAttribute("data-color", color);
+    cartDisplaySection.appendChild(newItemArticle);
 
+    let newItemDivImg = document.createElement('div');
+    newItemDivImg.classList.add("cart__item__img");
+    newItemArticle.appendChild(newItemDivImg);
+
+    let newItemImg = document.createElement('img');
+    newItemImg.setAttribute("src", singleProduct.imageUrl);
+    newItemImg.setAttribute("alt", singleProduct.altTxt);
+    newItemDivImg.appendChild(newItemImg);
+
+    let newItemDivContent = document.createElement('div');
+    newItemDivContent.classList.add("cart__item__content");
+    newItemArticle.appendChild(newItemDivContent);
+
+    let newItemDivDescription = document.createElement("div");
+    newItemDivDescription.classList.add("cart__item__content__description");
+    newItemDivContent.appendChild(newItemDivDescription);
+
+    let newItemHeading = document.createElement("h2");
+    newItemHeading.innerText = singleProduct.name;
+    newItemDivDescription.appendChild(newItemHeading);
+
+    let newItemParagraphColor = document.createElement("p");
+    newItemParagraphColor.innerHTML = color;
+    newItemDivDescription.appendChild(newItemParagraphColor);
+
+    let newItemParagraphPrice = document.createElement("p");
+    newItemParagraphPrice.innerHTML = singleProduct.price + " €";
+    newItemDivDescription.appendChild(newItemParagraphPrice);
+
+    let newItemDivContentSetting = document.createElement('div');
+    newItemDivContentSetting.classList.add("cart__item__content__settings");
+    newItemDivContent.appendChild(newItemDivContentSetting);
+
+    let newItemDivQuantity = document.createElement('div');
+    newItemDivQuantity.classList.add("cart__item__content__settings__quantity");
+    newItemDivContentSetting.appendChild(newItemDivQuantity);
+
+    let newItemParagraphQuantity = document.createElement("p");
+    newItemParagraphQuantity.innerText = "Qté : ";
+    newItemDivQuantity.appendChild(newItemParagraphQuantity);
+
+    let newItemInputQuantity = document.createElement("input");
+    newItemInputQuantity.classList.add("itemQuantity");
+    newItemInputQuantity.setAttribute("type", "number");
+    newItemInputQuantity.setAttribute("name", "itemQuantity");
+    newItemInputQuantity.setAttribute("min", "1");
+    newItemInputQuantity.setAttribute("max", "100");
+    newItemInputQuantity.setAttribute("value", quantity);
+    newItemDivQuantity.appendChild(newItemInputQuantity);
+
+    let newItemDivDelete = document.createElement("div");
+    newItemDivDelete.classList.add("cart__item__content__settings__delete");
+    newItemDivContentSetting.appendChild(newItemDivDelete);
+
+    let newItemParagraphDelete = document.createElement("p");
+    newItemParagraphDelete.classList.add("deleteItem");
+    newItemParagraphDelete.innerText = "Supprimer";
+    newItemDivDelete.appendChild(newItemParagraphDelete);
+    
+
+}
 
